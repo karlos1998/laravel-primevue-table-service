622ee132ab86d70ff3cd33f355007e2b94e57345
karlos1998 <fadeusz98@gmail.com>
2024-03-28 01:09:11 +0100

    try to add provider

---
+++ src/Providers/LaravelPrimevueTableServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Karlos3098\LaravelPrimevueTableService\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelPrimevueTableServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__.'/../Assets' => resource_path('js'),
+        ], 'public');
+    }
+}
