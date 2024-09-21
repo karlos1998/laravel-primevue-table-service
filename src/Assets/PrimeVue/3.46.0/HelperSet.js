dd1e30166d6ff2cba8446d056c0c9eb603c4f627
karlos1998 <fadeusz98@gmail.com>
2024-09-21 19:33:52 +0200

    remove console logs

--- src/Assets/PrimeVue/3.46.0/HelperSet.js
+++ src/Assets/PrimeVue/3.46.0/HelperSet.js
@@ -3,12 +3,10 @@ export default class {
     type;
 
     constructor({ init, type }) {
-        console.log('helper set constructor method')
         this.helpers = new Set(init);
         this.type = type;
     }
     add(instance) {
-        console.log('helper set add method')
         this.helpers.add(instance);
     }
     update() {
@@ -21,7 +19,6 @@ export default class {
         this.helpers.clear();
     }
     get(parentInstance, slots) {
-        console.log('helper set get method')
         return [...this.helpers].map((v) => v.vnode);
     }
 }
